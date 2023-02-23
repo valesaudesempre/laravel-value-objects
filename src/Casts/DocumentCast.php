21308f1f37ef523010b74fe4224a28e048a71cae
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2023-02-23 14:34:51 -0300

    feat(casts): permite valor null em DocumentCast

--- src/Casts/DocumentCast.php
+++ src/Casts/DocumentCast.php
@@ -13,8 +13,16 @@ class DocumentCast implements CastsAttributes
      * @param string                                                $value
      * @param array{document_type: string, document_number: string} $attributes
      */
-    public function get($model, string $key, $value, array $attributes): Document
+    public function get($model, string $key, $value, array $attributes): ?Document
     {
+        if (!isset($attributes['document_number']) && !isset($attributes['document_type'])) {
+            return null;
+        }
+
+        if (!isset($attributes['document_number'], $attributes['document_type'])) {
+            throw new InvalidArgumentException('Both document_number and document_type keys must be set or null.');
+        }
+
         return new Document(
             $attributes['document_number'],
             DocumentType::from($attributes['document_type'])
@@ -22,13 +30,17 @@ class DocumentCast implements CastsAttributes
     }
 
     /**
-     * @param Document             $value
+     * @param Document|null        $value
      * @param array<string, mixed> $attributes
      *
      * @return array{document_type: string, document_number: string}
      */
     public function set($model, string $key, $value, array $attributes): array
     {
+        if (!isset($value)) {
+            return ['document_number' => null, 'document_type' => null];
+        }
+
         if (!$value instanceof Document) {
             throw new InvalidArgumentException('The given value is not a Document instance.');
         }
